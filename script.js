aa2ade8a055a90f7e696c576e029395caef0930d
yucheng2017 <khjhjjh@hotmail.com>
2017-04-05 02:51:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+var addItemButton = document.getElementById("add-item");
+var addListButton = document.getElementById("add-list");
+
+var addListPopupDiv = document.getElementById("add-list-popup");
+var addItemPopupDiv = document.getElementById("add-item-popup");
+
+addListButton.addEventListener("click", function() {
+  addListPopupDiv.style.display = "flex";
+});
+addItemButton.addEventListener("click", function() {
+  addItemPopupDiv.style.display = "flex";
+});
+
+var closeButton = document.querySelectorAll(".close");
+ closeButton.forEach(function(button, i) {
+ button.addEventListener("click", function() {
+     addListPopupDiv.style.display = "none";
+     addItemPopupDiv.style.display = "none";
+  });
+});
+
